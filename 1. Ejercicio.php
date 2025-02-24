9fe64bd2e66cb72374f6b4fdf287f8b11dc2ed6a
Mayeli <mayeli.carhuaricra10@gmail.com>
2025-02-24 11:00:40 +0100

    Ejercicio_1

---
+++ 1. Ejercicio.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio_1</title>
+</head>
+<body>
+
+    <?php
+      function imprimir_resultado($texto, $resultado) {
+        echo "$texto: $resultado <br>";
+      }
+
+      imprimir_resultado("Nombre" , "Liliana");
+      imprimir_resultado("Apellido" , "Mercedes");
+      imprimir_resultado("Dirección" , "Calle 124");
+    ?>
+
+</body>
+</html>
