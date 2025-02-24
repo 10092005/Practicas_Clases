c497081b36c09ce6712246625b3332e8af71638c
Mayeli <mayeli.carhuaricra10@gmail.com>
2025-02-24 13:35:57 +0100

    commit

--- 6_Ejercicio.php
+++ 6_Ejercicio.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ejercicio_6</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="6_style.css">
 </head>
 <body>
     <h1>Viaje a Las Islas Canarias</h1>
