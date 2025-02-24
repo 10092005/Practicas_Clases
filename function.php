5d94cf940c500d0659d57c81718542b25e1f8791
Mayeli <mayeli.carhuaricra10@gmail.com>
2025-02-24 13:01:51 +0100

    style

--- function.php
+++ function.php
@@ -18,17 +18,17 @@
             if ($fecha === $fecha_hoy) {
                 if ($edad > 50) {
                     $precio = $precio_base / 2;
-                    echo "El viaje te sale a mitad de precio!"
+                    echo "El viaje te sale a mitad de precio! <br>";
                 } elseif ($edad < 15) {
                     $precio = 0; 
-                    echo "La super oferta. El viaje te sale gratis!!"
+                    echo "La super oferta. El viaje te sale gratis!! <br>";
                 } else {
                     $precio = $precio_base;
-                    echo "No tienes ningun descuento"
+                    echo "No tienes ningun descuento <br>";
                 }
             } else {
                 $precio = $precio_base; 
-                echo "Ya no aplican descuentos"
+                echo "Ya no aplican descuentos <br>";
             }
             
             echo "Precio del viaje: " . $precio . "€";
--- index.php
+++ index.php
@@ -4,16 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ejercicio_6</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="function.php">
-        <label for="edad">Edad: </label>
-        <input type="number" name="edad" required>
+    <h1>Viaje a Las Islas Canarias</h1>
+    <h3>Verifica si obtienes algun descuento</h3>
+    <h5>OJO: Solo por hoy descuentos</h5>
+    <form method="GET" action="function.php">
+        <label for="edad" class="edadLab">Edad: </label>
+        <input type="number" name="edad" id="edad" required>
 
-        <label for="fecha">Fecha: </label>
-        <input type="date" name="fecha" value="<?php echo date('Y-m-d'); ?>" required>
+        <label for="fecha" class="fechaLab">Fecha: </label>
+        <input type="date" name="fecha" id="fecha" value="<?php echo date('Y-m-d'); ?>" required>
 
-        <button type="submit">Calcular Precio</button>
+        <button type="submit" class="button">Calcular</button>
     </form>
 </body>
 </html>
