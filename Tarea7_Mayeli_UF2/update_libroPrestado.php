5097643aba3f3ed8c7c483553983e651d8f7eace
Mayeli <mayeli.carhuaricra10@gmail.com>
2025-04-04 12:05:52 +0200

    delete

--- Tarea7_Mayeli_UF2/update_libroPrestado.php
+++ Tarea7_Mayeli_UF2/update_libroPrestado.php
@@ -2,6 +2,7 @@
     require_once 'conexion.php';
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
         if (isset($_POST["libro"])) {
             $libro_id = $_POST["libro"];
             $nombre = $_POST["nombre"];
@@ -10,7 +11,7 @@
 
             $sql = "INSERT INTO prestamos (nombre, correo, libro_id, tiempo) VALUES ('$nombre', '$correo', '$libro_id', '$tiempo')";
             if (mysqli_query($conexion, $sql)) {
-                $sqlUpdate = "UPDATE libros SET estado = 0 WHERE id = $libro_id";
+                $sqlUpdate = "UPDATE libros SET estado = 1 WHERE id = $libro_id";
                 if (mysqli_query($conexion, $sqlUpdate)) {
                     header("Location: index.php");
                     exit();
@@ -21,7 +22,7 @@
                 echo "Error al insertar el préstamo: " . mysqli_error($conexion);
             }
         } else {
-            echo "Error: No se proporcionó el 'libro_id'.";
+            echo "Error: No se proporcionó el 'libro'.";
         }
     }
 ?>
